fed79fa64e28741b6488fea696a43d58f83ab717
Joseph Talon <josephtalon01@gmail.com>
2023-11-08 23:21:30 -0500

    map

--- src/lib/Map.svelte
+++ src/lib/Map.svelte
@@ -2,10 +2,15 @@
   import { Map, View } from "ol";
   import { useGeographic } from "ol/proj.js";
   import OSM from "ol/source/OSM.js";
-  import TileLayer from "ol/layer/Tile.js";
   import Control from "ol/control/Control.js";
+  import Feature from "ol/Feature.js";
+  import Point from "ol/geom/Point.js";
+  import LineString from "ol/geom/LineString.js"
+  import { Icon, Stroke, Style } from "ol/style.js";
+  import { Tile as TileLayer, Vector as VectorLayer } from "ol/layer.js";
+  import { Vector as VectorSource } from "ol/source.js"
   import { onMount } from "svelte";
-  import { searchAddress } from "./utils.js";
+  import { searchAddress, reqRoute, parseRoute, exportJson } from "./utils.js";
 
   useGeographic();
 
@@ -14,11 +19,22 @@
   let map;
   let position;
   let addressSearch;
+  let featureLayer
+  let featureLayerEnd
+  let startFeature
+  let endFeature
+  let routeFeature
 
   let startSuggestions;
   let endSuggestions;
   let routeStart;
   let routeEnd;
+  let routeLayer
+  let routeRes
+
+  const rasterLayer = new TileLayer({
+    source: new OSM(),
+  });
 
   let locSuccess = (position) => {
     position = position;
@@ -41,11 +57,7 @@
       zoom: 15,
     });
     map = new Map({
-      layers: [
-        new TileLayer({
-          source: new OSM(),
-        }),
-      ],
+      layers: [rasterLayer],
       target: target,
       view: view,
       controls: [addressSearchControl],
@@ -67,12 +79,67 @@
       endSuggestions = addRes;
       startSuggestions = null;
     }
-    console.log(addRes);
   }
 
   function setStart(index) {
     routeStart = startSuggestions[index];
-    console.log(routeStart);
+    startSuggestions = null
+
+    map.removeLayer(featureLayer)
+    startFeature = new Feature(new Point([parseFloat(routeStart.lon), parseFloat(routeStart.lat)]))
+    featureLayer = new VectorLayer({
+      className: "startLayer",
+      source: new VectorSource({
+        features: [startFeature]
+      }),
+      style: new Style({
+        image: new Icon({
+          src: 'startMarker.svg',
+          scale: 0.2
+        })
+      })
+    })
+    map.addLayer(featureLayer)
+  }
+
+  function setEnd(index) {
+    routeEnd = endSuggestions[index];
+    endSuggestions = null;
+
+    map.removeLayer(featureLayerEnd)
+    endFeature = new Feature(new Point([parseFloat(routeEnd.lon), parseFloat(routeEnd.lat)]))
+    featureLayerEnd = new VectorLayer({
+      className: "endLayer",
+      source: new VectorSource({
+        features: [endFeature]
+      }),
+      style: new Style({
+        image: new Icon({
+          src: 'endMarker.svg',
+          scale: 0.2
+        })
+      })
+    })
+    map.addLayer(featureLayerEnd)
+  }
+
+  async function findRoute() {
+    routeRes = await reqRoute(routeStart.lon, routeStart.lat, routeEnd.lon, routeEnd.lat) 
+
+    map.removeLayer(routeLayer)
+    routeFeature = new Feature(new LineString(routeRes.routes[0].geometry.coordinates))
+    routeLayer = new VectorLayer({
+      source: new VectorSource({
+        features: [routeFeature]
+      }),
+      style: new Style({
+        stroke: new Stroke({
+          color: '#3268ab',
+          width: 4
+        })
+      })
+    })
+    map.addLayer(routeLayer)
   }
 </script>
 
@@ -96,12 +163,28 @@
       <label for="endPoint">End</label>
       <input name="endPoint" />
     </form>
+    {#if routeStart && routeEnd}
+      <button on:click={findRoute}>Find Route</button>
+    {/if}
+    {#if routeRes}
+      <button><a href={exportJson(parseRoute(routeRes))} download="route.json">Download route.json</a></button>
+    {/if}
   </div>
 
   {#if startSuggestions}
     <div class="pointSelect">
       {#each startSuggestions as sug, i}
         <div sug-id="i" class="suggestion" on:click={() => setStart(i)}>
+          {sug.display_name}
+        </div>
+      {/each}
+    </div>
+  {/if}
+
+  {#if endSuggestions}
+    <div class="pointSelect">
+      {#each endSuggestions as sug, i}
+        <div sug-id="i" class="suggestion" on:click={() => setEnd(i)}>
           {sug.display_name}
         </div>
       {/each}
@@ -147,6 +230,7 @@
   }
 
   .suggestion {
+    color: white;
     padding: 5px;
     border-top: 1px solid white;
     border-left: 1px solid white;
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -3,4 +3,28 @@ async function searchAddress(query) {
   return res.json()
 }
 
-export { searchAddress }
+async function reqRoute(lonStart, latStart, lonEnd, latEnd) {
+  const res = await fetch(`http://router.project-osrm.org/route/v1/cycling/${lonStart},${latStart};${lonEnd},${latEnd}?geometries=geojson&steps=true`)
+  return res.json()
+}
+
+function parseRoute(routeData) {
+  let route = routeData.routes[0]
+  let routeGeo = route.geometry.coordinates
+  let routeDistance = route.distance
+  let routeSteps = route.legs[0].steps
+  
+  let output = {
+    geo: routeGeo,
+    distance: routeDistance,
+    steps: routeSteps
+  }
+
+  return output
+}
+
+function exportJson(jsonObj) {
+  return "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(jsonObj))
+}
+
+export { searchAddress, reqRoute, parseRoute, exportJson }
